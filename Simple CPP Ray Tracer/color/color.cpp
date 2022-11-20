b8b2cf96f26f374c1a802c2d326e74916bb3d6f6
Wiktor Zając <ogniko00@gmail.com>
2022-11-20 16:40:50 +0100

    Added: RGB color header and implementation

---
+++ Simple CPP Ray Tracer/color/color.cpp
@@ -0,0 +1,8 @@
+#include "color.hpp"
+
+RGB::RGB(byte r, byte g, byte b)
+{
+	this->R = r;
+	this->G = g;
+	this->B = b;
+}
---
+++ Simple CPP Ray Tracer/color/color.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../types.hpp"
+
+struct RGB {
+	byte R = 0, G = 0, B = 0;
+
+	RGB(byte r = 0, byte g = 0, byte b = 0);
+};
+
+
