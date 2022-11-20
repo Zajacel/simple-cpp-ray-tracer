6f29aa33030719634ceee390982101ea954bc624
Wiktor Zając <ogniko00@gmail.com>
2022-11-20 16:44:59 +0100

    Added: Project configs

---
+++ Simple CPP Ray Tracer/Simple CPP Ray Tracer.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+#include "types.hpp"
+#include "exporter/ppm.hpp"
+
+#define WIDTH 4;
+#define HEIGHT 4;
+
+int main()
+{
+	const int X = WIDTH;
+	const int Y = HEIGHT;
+	auto image = new RGB[Y][X];
+
+	for (unsigned int y = 0; y < Y; y++)
+		for (unsigned int x = 0; x < X; x++) {
+			RGB t = RGB(128, 128, 128);
+			image[y][x] = t;
+		}
+
+	exportPPMbin(*image, v3(X, Y, 0));
+    return 0;
+}
